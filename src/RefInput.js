6e0e5d9b7ddfee584e99a7fd57f9d89be119c29f
kurokeita <kuro.keita94@gmail.com>
2020-02-10 18:12:37 +0700

    feature/forward_ref Using class based component and helper to forward ref instead of purely function based component

--- src/RefInput.js
+++ src/RefInput.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import FormControl from 'react-bootstrap/FormControl'
 
-const RefInput = React.forwardRef((props, ref) => (
-        <FormControl name={props.name} ref={ref} value={props.value} onChange={props.onChange}/>
-))
+// const RefInput = React.forwardRef((props, ref) => (
+//         <FormControl name={props.name} ref={ref} value={props.value} onChange={props.onChange}/>
+// ))
 
-export default RefInput
+class RefInput extends React.Component {
+    render() {
+        return (
+            <FormControl name={this.props.name} ref={this.props.innerRef} value={this.props.value} onChange={this.props.onChange}/>
+        )
+    }
+}
+
+export default React.forwardRef((props, ref) => (
+    <RefInput innerRef={ref} {...props}/>
+))

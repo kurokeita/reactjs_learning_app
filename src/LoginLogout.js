c0765119139cbc432435cf6591b9fecbd9635bc9
kurokeita <kuro.keita94@gmail.com>
2020-02-13 10:50:57 +0700

    feature/hook_useState_useContext rewrote LoginLogout using function based component and state hook

--- src/LoginLogout.js
+++ src/LoginLogout.js
@@ -1,51 +1,76 @@
-import React from 'react'
-import {ThemeContext} from './ThemeContext'
+import React, { useState } from 'react'
+// import {ThemeContext} from './ThemeContext'
 import Button from 'react-bootstrap/Button'
 // const axios = require('axios')
 
-class LoginLogout extends React.Component {
-    static contextType = ThemeContext
-    constructor(props) {
-        super(props)
-        this.state = {
-            isLoggedIn: false
-        }        
-        this.handleClick = this.handleClick.bind(this)
-    }
+// class LoginLogout extends React.Component {
+//     static contextType = ThemeContext
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             isLoggedIn: false
+//         }
+//         this.handleClick = this.handleClick.bind(this)
+//     }
+//
+//     handleClick(e) {
+//         let name = e.target.name
+//         let status = (name === 'login')
+//         let username = (status === true) ? 'kurokeita' : ''
+//         this.setState({
+//             isLoggedIn: status,
+//         })
+//         this.props.loggedInStatus(status, username)
+//         // axios.request({
+//         //     url: 'http://kurokeita.ddns.net:38080/api/test',
+//         //     headers: {
+//         //         'Access-Control-Allow-Origin': '*',
+//         //     },
+//         //     method: 'get'
+//         // }).then((res) => {
+//         //     console.log(res)
+//         // })
+//     }
+//
+//     render() {
+//         let button
+//         if (!this.state.isLoggedIn) {
+//             button = <Button className="mb-3" variant="primary" name="login" onClick={this.handleClick}>Log in</Button>
+//         } else {
+//             button = <Button className="mb-3" variant="danger" name="logout" onClick={this.handleClick}>Log out</Button>
+//         }
+//
+//         return(
+//             <div>
+//                 {button}
+//             </div>
+//         )
+//     }
+// }
 
-    handleClick(e) {
+function LoginLogout(props) {
+    const [ loginState, setLoginState ] = useState(false)
+
+    function handleClick(e) {
         let name = e.target.name
         let status = (name === 'login')
         let username = (status === true) ? 'kurokeita' : ''
-        this.setState({
-            isLoggedIn: status,
-        })
-        this.props.loggedInStatus(status, username)
-        // axios.request({
-        //     url: 'http://kurokeita.ddns.net:38080/api/test',
-        //     headers: {
-        //         'Access-Control-Allow-Origin': '*',
-        //     },
-        //     method: 'get'
-        // }).then((res) => {
-        //     console.log(res)
-        // })
+        setLoginState(status)
+        props.loggedInStatus(status, username)
     }
 
-    render() {
-        let button
-        if (!this.state.isLoggedIn) {
-            button = <Button className="mb-3" variant="primary" name="login" onClick={this.handleClick}>Log in</Button>
-        } else {
-            button = <Button className="mb-3" variant="danger" name="logout" onClick={this.handleClick}>Log out</Button>
-        }
-
-        return(
-            <div>
-                {button}
-            </div>
-        )
+    let button
+    if (!loginState) {
+        button = <Button className="mb-3" variant="primary" name="login" onClick={handleClick}>Log in</Button>
+    } else {
+        button = <Button className="mb-3" variant="danger" name="logout" onClick={handleClick}>Log out</Button>
     }
+
+    return(
+        <div>
+            {button}
+        </div>
+    )
 }
 
 export default LoginLogout

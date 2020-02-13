24ecfea34aec3d04177f3713c4b73d0ace464f60
kurokeita <kuro.keita94@gmail.com>
2020-02-13 15:17:27 +0700

    feature/hook_useState_useContext rewrite Time component in function and use effect hook to set interval

--- src/Time.js
+++ src/Time.js
@@ -1,31 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-class Time extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {time: new Date()}
-    }
+// class Time extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {time: new Date()}
+//     }
+//
+//     componentDidMount() {
+//         this.timerID = setInterval(
+//             () => this.tick(),
+//             this.props.interval
+//         )
+//     }
+//
+//     componentWillUnmount() {
+//         clearInterval(this.timerID)
+//     }
+//
+//     tick() {
+//         this.setState({
+//             time: new Date()
+//         })
+//     }
+//
+//     render() {
+//         return <span>The time is: {this.state.time.toLocaleTimeString()}</span>
+//     }
+// }
 
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            this.props.interval
+function Time(props) {
+    const [ time, setTime ] = useState((new Date()).toLocaleTimeString())
+
+    useEffect(() => {
+        const timer = setInterval(
+            () => {
+                setTime((new Date()).toLocaleTimeString())
+            },
+            props.interval
         )
-    }
+        return () => {
+            clearInterval(timer)
+        }
+    })
 
-    componentWillUnmount() {
-        clearInterval(this.timerID)
-    }
-
-    tick() {
-        this.setState({
-            time: new Date()
-        })
-    }
-
-    render() {
-        return <span>The time is: {this.state.time.toLocaleTimeString()}</span>
-    }
+    return(
+        <span>The time is: {time}</span>
+    )
 }
 
 export default Time
